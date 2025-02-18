9b4235132b83b9a3a2ad6502ddcaea57e56c4cec
khanhduong <duongkhanhb1k39@gmail.com>
2025-02-18 23:30:16 +0700

    init middle ware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require("express")
+const morgan = require("morgan")
+// const compression = require("compression")
+const helmet    = require("helmet");
+const compression = require("compression");
+
 const app = express();
 
 //init middle ware
 
+app.use(morgan("dev"))
+app.use(helmet());
+// app.use(compression());
+app.use(compression());
 //init db
 
 
